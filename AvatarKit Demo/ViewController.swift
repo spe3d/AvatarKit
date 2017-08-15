00e313006af6ec5f60946199cb6b4ee3d778e88f
Daniel Lee <kokuyoku82@gmail.com>
2017-08-15 15:47:27 +0800

    新增 Demo 專案

---
+++ AvatarKit Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvatarKit Demo
+//
+//  Created by Hao Lee on 2017/8/15.
+//  Copyright © 2017年 Speed 3D Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
