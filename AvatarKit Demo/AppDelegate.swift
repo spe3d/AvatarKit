6dad9faf770e751492eb711c551e2b32122434fa
Daniel Lee <kokuyoku82@gmail.com>
2017-08-16 15:17:58 +0800

    嘗試於 APP 內呼叫 AvatarKit

--- AvatarKit Demo/AppDelegate.swift
+++ AvatarKit Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AvatarKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        AvatarKit.start(withAPIKey: "")
+        
         return true
     }
 
