2a3d78e5c7ecfc224a506a2b364ccb6a0911c923
Daniel Lee <kokuyoku82@gmail.com>
2017-08-17 11:01:18 +0800

    use shell to download opencv

---
+++ DownloadOpenCV.sh
@@ -0,0 +1,4 @@
+rm -r AvatarKit/opencv2.framework
+curl -L -O https://github.com/kylefleming/opencv/releases/download/3.1.0-ios-fix/opencv2.framework.zip
+unzip opencv2.framework.zip -d AvatarKit
+rm opencv2.framework.zip
