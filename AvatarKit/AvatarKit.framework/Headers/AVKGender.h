87cc381ffa554d243895946c69537b742344a129
Speed3D Jenkins <dev@spe3d.co>
2017-11-02 14:59:28 +0800

    Release new Framework

--- AvatarKit/AvatarKit.framework/Headers/AVKGender.h
+++ AvatarKit/AvatarKit.framework/Headers/AVKGender.h
@@ -10,15 +10,21 @@
 
 /**
  The gender of avatar.
+ 
+ 公仔的性別。
  */
 typedef NS_ENUM(NSUInteger, AVKGender) {
     /**
      The gender is male.
+     
+     此性別為男性。
      */
     AVKGenderMale = 0,
     
     /**
      The gender is female.
+     
+     此性別為女性。
      */
     AVKGenderFemale = 1,
 };
@@ -26,9 +32,12 @@ typedef NS_ENUM(NSUInteger, AVKGender) {
 /**
  Returns a string representation of a gender.
  
+ 回傳性別所代表的文字。
+ 
  @param gender
- The enum of gender.
+ The gender. 性別參數。
  
  @return Returns a symbol string of the gender. The male is `M`. The female is `F`.
+ 回傳性別所代表的文字。男性為`M`。女性為`F`。
  */
 NSString * _Nonnull NSStringFromAVKGender(AVKGender gender);
